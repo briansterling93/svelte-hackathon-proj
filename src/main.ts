a86e7109647b1015478c654e6da1ccc023c8740a
William Bryce Angell <bryce.angell@resi.io>
2022-12-12 11:57:31 -0600

    get app hooked up to Studio

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+(window as any).renderPaneViewCurrentOrg = (containerId: string) => {
+  new App({
+    target: document.getElementById(containerId),
+  });
+};
 
-export default app;
+(window as any).unmountPaneViewCurrentOrg = (containerId: string) => {
+  new App({
+    target: document.getElementById(containerId),
+  }).$destroy();
+};
